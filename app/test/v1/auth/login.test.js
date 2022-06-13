fb827c54911bb38a63b347c5581d2497cdce927b
narantyo maulana <olandeoxys@gmail.com>
2022-06-13 14:18:05 +0700

    [ADD]login.test.js

---
+++ app/test/v1/auth/login.test.js
@@ -0,0 +1,54 @@
+const request = require('supertest');
+const app = require('../../..');
+
+describe('POST /v1/auth/login', () => {
+  const email = 'johnny@binar.co.id';
+  const password = '123456';
+  const wrongEmail = 'invalid@binar.co.id';
+  const wrongPassword = 'invalid';
+
+  it('should response with 201 as status code', async () => request(app)
+    .post('/v1/auth/login')
+    .send({
+      email,
+      password,
+    })
+    .then((res) => {
+      expect(res.statusCode).toBe(201);
+      expect(res.body).toEqual({ accessToken: expect.any(String) });
+    }));
+
+  it('should response with 404 as status code', async () => request(app)
+    .post('/v1/auth/login')
+    .send({
+      email: wrongEmail,
+      password,
+    })
+    .then((res) => {
+      expect(res.statusCode).toBe(404);
+      expect(res.body).toEqual({
+        error: {
+          name: expect.any(String),
+          message: expect.any(String),
+          details: { email: expect.any(String) },
+        },
+      });
+    }));
+
+  it('should response with 401 as status code', async () => request(app)
+    .post('/v1/auth/login')
+    .send({
+      email,
+      password: wrongPassword,
+    })
+    .then((res) => {
+      expect(res.statusCode).toBe(401);
+      expect(res.body).toEqual({
+        error: {
+          name: expect.any(String),
+          message: expect.any(String),
+          details: expect.any(Object),
+        },
+      });
+    }));
+});
